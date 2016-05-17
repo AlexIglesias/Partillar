7b048291a8933828c394f1911cc68da20e02be33
Alex Iglesias <jandroiglesias@gmail.com>
2016-05-17 11:51:56 +0200

    routes modified

--- partillar/config/routes.rb
+++ partillar/config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   end
 
   resources :contents
-
+  root to: "contents#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
